126a86d38347eebab18967b1bbe73949e685fbd4
jojocatubay <53652824+jojocatubay@users.noreply.github.com>
2019-08-05 09:27:55 +0800

    Add Test unit project
    Add Test unit project

---
+++ VisualRiskNVPTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("VisualRiskNVPTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("VisualRiskNVPTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6cde1d01-c09f-438e-85c9-7a8e753820b2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ VisualRiskNVPTest/UnitTest1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VisualRiskNPV;
+using VisualRiskNPV.Model;
+using VisualRiskNPV.ViewModel;
+
+namespace VisualRiskNVPTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private MainViewModel mainViewModel;
+        [TestMethod]
+        public void OnCalculate()
+        {
+            //Bound and Rates
+            mainViewModel.LowerBoundRate = 10;
+            mainViewModel.UpperBoundRate = 20;
+            mainViewModel.Increment = Convert.ToDecimal(0.25);
+            mainViewModel.InitialValue = 10000;
+            //CashFlow
+            mainViewModel.CashFlow1 = 10000;
+            mainViewModel.CashFlow2 = 15000;
+            mainViewModel.CashFlow3 = 20000;
+
+            var initialValue = Convert.ToDouble(mainViewModel.InitialValue.ToString().Contains("-") ? mainViewModel.InitialValue : Convert.ToDecimal("-" + mainViewModel.InitialValue));
+            var diff = mainViewModel.UpperBoundRate - mainViewModel.LowerBoundRate;
+            int numberOfIncrement = Convert.ToInt32(diff / mainViewModel.Increment);
+            var totalCashFlow = mainViewModel.CashFlow1 + mainViewModel.CashFlow2 + mainViewModel.CashFlow3;
+
+            var ValueRate = Convert.ToDecimal(mainViewModel.Increment);
+            for (int i = 0; i < numberOfIncrement; i++)
+            {
+                var cfr = totalCashFlow / ValueRate;
+                var Npv = Convert.ToDecimal(initialValue) + Convert.ToDecimal(cfr);
+                Console.WriteLine(ValueRate + " || " + Npv);
+                ValueRate += mainViewModel.Increment;
+            }
+        }
+
+    }
+}
